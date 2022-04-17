066da4de7ae8628825218ffd53532cd4a776644f
Selim Enes Erdogan <sellim81@gmail.com>
2022-04-17 18:46:49 +0300

    header updates
    basic header updates

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './header.css';
 import {GiHamburgerMenu} from 'react-icons/gi';
+import WebteknoLogo from '../../images/webtekno-logo-white.png';
+import {GoTriangleDown} from 'react-icons/go'
 
 const index = () => {
   return (
     <div className='header'>
-      <div>
+      <div className='header-left'>
         <GiHamburgerMenu className='hamburger-menu-icon' size={20}/>
+        <img src={WebteknoLogo} alt="logo" className='wb-logo'/>
+        <ul>
+          <li>Anasayfa</li>
+          <li>Haber</li>
+          <li>Video</li>
+          <li>Kategoriler <GoTriangleDown/></li>
+        </ul>
       </div>
     </div>
   )

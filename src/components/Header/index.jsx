aec0bb16312d7999eec625f9a8dfa6dc35621e8d
Selim Enes Erdogan <sellim81@gmail.com>
2022-04-17 19:33:07 +0300

    header-right added
    right side of the header added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import './header.css';
 import {GiHamburgerMenu} from 'react-icons/gi';
 import WebteknoLogo from '../../images/webtekno-logo-white.png';
-import {GoTriangleDown} from 'react-icons/go'
+import {GoTriangleDown, GoSearch} from 'react-icons/go';
+import {FaUser} from 'react-icons/fa';
+import {MdFavorite} from 'react-icons/md';
+
 
 const index = () => {
   return (
     <div className='header'>
       <div className='header-left'>
-        <GiHamburgerMenu className='hamburger-menu-icon' size={20}/>
+        <GiHamburgerMenu className='hamburger-menu-icon' size={32}/>
         <img src={WebteknoLogo} alt="logo" className='wb-logo'/>
         <ul>
           <li>Anasayfa</li>
@@ -16,6 +19,11 @@ const index = () => {
           <li>Video</li>
           <li>Kategoriler <GoTriangleDown/></li>
         </ul>
+      </div>
+      <div className="header-right">
+        <GoSearch size={28} className='search-icon'/>
+        <p className='header-login'><FaUser className='user-icon' size={28}/> Giriş Yap</p>
+        <MdFavorite size={28} className='favs-icon'/>
       </div>
     </div>
   )

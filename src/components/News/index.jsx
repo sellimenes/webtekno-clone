519ed14c9eb8eb9e79a4f98a8e104145fda4f8fd
Selim Enes Erdogan <sellim81@gmail.com>
2022-04-18 23:01:34 +0300

    latest news card design
    created latest news single card design

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import './news.css';
+import {MdPhoneIphone} from 'react-icons/md';
+import News1 from '../../images/posts/news-1.jpg';
 
 const index = () => {
   return (
-    <div>
-      <h3>Yayın Akışı</h3>
-      <div className="news">
-        <div className="news-icon"></div>
-        <img src="" alt="news image" />
-        <div className="news-content">
-          <p>NEWS TAG</p>
-          <h2>iPhone 14 Tanıtıldıktan Sonra Hangi iPhone Modellerine Veda Edeceğiz?</h2>
-          <p>Author Name</p>
+    <div className='container'>
+      <h3 className='yayin-akisi'>Latest News</h3>
+      <div className="news-wrapper">
+        <div className="news">
+          <div className='news-left'>
+            <MdPhoneIphone size={42} className="news-icon"/>
+            <span>1 Hour Ago</span>
+          </div>
+          <img src={News1} alt="news image" className='news-img'/>
+          <div className="news-content">
+            <p className='news-tag'>NEWS TAG</p>
+            <h2>iPhone 14 Tanıtıldıktan Sonra Hangi iPhone Modellerine Veda Edeceğiz?</h2>
+            <p className='news-author'>Author Name</p>
+          </div>
+        </div>
+        <div className="news">
+          <div className='news-left'>
+            <MdPhoneIphone size={42} className="news-icon"/>
+            <span>1 Hour Ago</span>
+          </div>
+          <img src={News1} alt="news image" className='news-img'/>
+          <div className="news-content">
+            <p className='news-tag'>NEWS TAG</p>
+            <h2>Tarih Boyunca Dünya Ekonomisini Şekillendiren Altın Fiyatları Aslında Neye Göre Belirleniyor?</h2>
+            <p className='news-author'>Author Name</p>
+          </div>
         </div>
       </div>
     </div>

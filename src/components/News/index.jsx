0a36db74654934a1c4bc7a21c47b461aab921d0d
Selim Enes Erdogan <sellim81@gmail.com>
2022-04-18 16:03:08 +0300

    slider responsive changes final

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -3,7 +3,18 @@ import './news.css';
 
 const index = () => {
   return (
-    <div>news</div>
+    <div>
+      <h3>Yayın Akışı</h3>
+      <div className="news">
+        <div className="news-icon"></div>
+        <img src="" alt="news image" />
+        <div className="news-content">
+          <p>NEWS TAG</p>
+          <h2>iPhone 14 Tanıtıldıktan Sonra Hangi iPhone Modellerine Veda Edeceğiz?</h2>
+          <p>Author Name</p>
+        </div>
+      </div>
+    </div>
   )
 }
 

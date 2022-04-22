c59e74a3eec6c68d15a890b657c31a80d15080b9
Selim Enes Erdogan <sellim81@gmail.com>
2022-04-22 15:53:54 +0300

    social cards designed

--- src/components/SocialCards/index.jsx
+++ src/components/SocialCards/index.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 import './socialcards.css'
-import {AiFillFacebook} from 'react-icons/ai';
+import {AiFillFacebook, AiFillTwitterSquare, AiFillInstagram, AiFillYoutube} from 'react-icons/ai';
 
 const index = () => {
   return (
-    <div>
+    <div className='cards'>
         <div className="single-card">
-            <div className="card-header">
-                <AiFillFacebook className='social-icon-facebook'/>
+            <div className="card-header card-facebook">
+                <AiFillFacebook className='social-icon'/>
             </div>
             <div className="card-content">
                 <div className='social-counter'>574B</div>
                 <div className='social-cta'>Beğeni</div>
+            </div>
+        </div>
+        <div className="single-card">
+            <div className="card-header card-twitter">
+                <AiFillTwitterSquare className='social-icon'/>
+            </div>
+            <div className="card-content">
+                <div className='social-counter'>370B</div>
+                <div className='social-cta'>Takipçi</div>
+            </div>
+        </div>
+        <div className="single-card">
+            <div className="card-header card-instagram">
+                <AiFillInstagram className='social-icon'/>
+            </div>
+            <div className="card-content">
+                <div className='social-counter'>1.7M</div>
+                <div className='social-cta'>Takipçi</div>
+            </div>
+        </div>
+        <div className="single-card">
+            <div className="card-header card-youtube">
+                <AiFillYoutube className='social-icon'/>
+            </div>
+            <div className="card-content">
+                <div className='social-counter'>3.18M</div>
+                <div className='social-cta'>Abone</div>
             </div>
         </div>
     </div>
